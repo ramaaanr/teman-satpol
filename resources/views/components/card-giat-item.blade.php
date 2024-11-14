e66e90e99dff6ba8b32653b22af2192ad74602b8
Rama Noor Rizki <ramanoorrizki@gmail.com>
2024-11-14 23:42:57 +0800

    feat: data giat consume

--- resources/views/components/card-giat-item.blade.php
+++ resources/views/components/card-giat-item.blade.php
@@ -3,41 +3,17 @@
 
   <div class="detail-1 flex">
     <div class="header-detail w-full">
-      <p class="font-bold text-3xl">Kegiatan Memilanduk</p>
-      <p class=" text-md text-zinc-500">memilanduk dengan sahabat</p>
+      <p class="font-bold text-3xl">{{$kegiatan}}</p>
+      <p class=" text-md text-zinc-500">{{$detailKegiatan}}</p>
     </div>
     <div class="actions-detail flex items-center">
-      <div class="text-4xl font-bold petugas-total-actions flex items-center">
-        <p class="text-4xl font-bold total-dikerjakan text-gray-300">12</p>
+      <div class="text-4xl font-bold petugas-total-actions flex items-center mr-2">
+        <p class="text-4xl font-bold total-dikerjakan text-gray-300">{{$jumlahSelesai}}</p>
         <p class="text-4xl font-bold ">/</p>
-        <p class="text-4xl font-bold total-target text-yellow-300 mr-1">120</p>
+        <p class="text-4xl font-bold total-target text-yellow-300 mr-1">{{$jumlahDitugaskan}}</p>
         <p class="text-sm font-semibold  leading-none">Mengisi Giat</p>
       </div>
-      <button id="dropdownMenuIconButton" data-dropdown-toggle="dropdownDots"
-        class="inline-flex items-center ml-2 border border-gray-300 p-2 text-sm font-medium text-center text-gray-400 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        type="button">
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-          viewBox="0 0 4 15">
-          <path
-            d="M3.5 1.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 6.041a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Zm0 5.959a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
-        </svg>
-      </button>
-
-      <!-- Dropdown menu -->
-      <div id="dropdownDots"
-        class="z-10 hidden bg-white divide-y border border-gray-200 divide-gray-100 rounded-lg shadow w-32 dark:bg-gray-700 dark:divide-gray-600">
-        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconButton">
-
-          <li>
-            <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Edit</a>
-          </li>
-          <li>
-            <a href="#"
-              class="block px-4 py-2 text-red-500 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Hapus</a>
-          </li>
-        </ul>
-      </div>
-
+      <x-button text="Detail" color='gray' />
     </div>
   </div>
 
@@ -46,13 +22,13 @@
       <span class="material-symbols-outlined mr-1 text-gray-300">
         location_on
       </span>
-      <p class="text-sm text-gray-700">Jl. Veteran Gang Kelurahan blbladpiadoiah</p>
+      <p class="text-sm text-gray-700">{{$tempat}}</p>
     </div>
     <div class="detail-content flex items-start w-full">
       <span class="material-symbols-outlined mr-1 text-gray-300">
         calendar_month
       </span>
-      <p class="text-sm text-gray-700">19 September 2024, 08:00 WITA - 19 September 2024 24:00 WITA</p>
+      <p class="text-sm text-gray-700">{{$tanggalMulai}}</p>
     </div>
   </div>
   <div class="detail-3 mt-2 flex">
@@ -60,19 +36,19 @@
       <span class="material-symbols-outlined mr-1 text-gray-300">
         lock_clock
       </span>
-      <p class="text-sm text-gray-700">19 September 2024, 08:00 WITA - 19 September 2024 24:00 WITA</p>
+      <p class="text-sm text-gray-700">{{$aksesMulai}}</p>
     </div>
     <div class="detail-content flex items-start w-1/4">
       <span class="material-symbols-outlined mr-1 text-gray-300">
         groups
       </span>
-      <p class="text-sm text-gray-700">120 petugas</p>
+      <p class="text-sm text-gray-700">{{$jumlahDitugaskan}} petugas</p>
     </div>
     <div class="detail-content flex items-start w-1/4">
       <span class="material-symbols-outlined mr-1 text-gray-300">
         airport_shuttle
       </span>
-      <p class="text-sm text-gray-700">Mobil Dinas</p>
+      <p class="text-sm text-gray-700">{{$kendaraan}}</p>
     </div>
   </div>
 </div>
