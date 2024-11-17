e559b30d8fbab29d577b5e6b519c22c899be639f
Rama Noor Rizki <ramanoorrizki@gmail.com>
2024-11-17 16:23:02 +0800

    feat: add laporan bidang

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,8 +2,17 @@ import "./bootstrap";
 import "flowbite";
 import Alpine from "alpinejs";
 import DataTable from "datatables.net-dt";
+import { Chart, registerables } from "chart.js"; // Pastikan registerables diimpor
 
-let table = new DataTable("#myTable");
+// Registrasi Chart.js components
+Chart.register(...registerables);
+
+// Inisialisasi Alpine.js
 window.Alpine = Alpine;
-
 Alpine.start();
+
+// Inisialisasi DataTable
+new DataTable("#myTable");
+
+// Ekspos Chart.js ke window agar bisa diakses dari luar
+window.Chart = Chart;
